8218d4e24fee8fa66b32f9d6ff1c2cc14ac513f3
Jacob Dodd <doddj@duq.edu>
2024-05-25 12:10:11 -0400

    save states

--- play-life.c
+++ play-life.c
@@ -1,5 +1,6 @@
 /*** includes ***/
 
+#include <bits/types/struct_iovec.h>
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
 #define _GNU_SOURCE
@@ -7,6 +8,7 @@
 #include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +25,29 @@
   { NULL, 0 }
 #define ROW_INIT                                                               \
   { 0, NULL }
+#define DFLT_MSG "HELP: Ctrl-Q = quit | Ctrl-S = save"
+
+/*** enums ***/
+
+enum key {
+  BACKSPACE = 127,
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
+
+/*** prototypes ***/
+
+void setStatusMessage(const char *fmt, ...);
+void refreshDisplay();
+int readKeyboardInput();
+char *promptUser(char *prompt);
 
 /*** members ***/
 
@@ -37,7 +62,9 @@ struct display {
   int cols;
   int cnt;
   int numRows;
+  int tempnumRows;
   grow *row;
+  grow *temprow;
   char *filename;
   char statusmsg[80];
   time_t status_time;
@@ -113,6 +140,25 @@ void die(const char *s) {
 
 /*** file i/o ***/
 
+// Convert array of grow structs to a single string buffer to be saved to disk
+char *rowsToString(int *len) {
+  int totlen = 0;
+  for (int i = 0; i < d.numRows; i++) {
+    totlen += d.row[i].size + 1;
+  }
+  *len = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (int i = 0; i < d.numRows; i++) {
+    memcpy(p, d.row[i].chars, d.row[i].size);
+    p += d.row[i].size;
+    *p = '\n';
+    p++;
+  }
+  return buf;
+}
+
 void appendRow(char *s, size_t len) {
   d.row = realloc(d.row, sizeof(grow) * (d.numRows + 1));
   int i = d.numRows;
@@ -123,9 +169,35 @@ void appendRow(char *s, size_t len) {
   d.numRows++;
 }
 
+void appendRowTemp(char *s, size_t len) {
+  d.temprow = realloc(d.temprow, sizeof(grow) * (d.tempnumRows + 1));
+  int i = d.tempnumRows;
+  d.temprow[i].size = len;
+  d.temprow[i].chars = malloc(len + 1);
+  memcpy(d.temprow[i].chars, s, len);
+  d.temprow[i].chars[len] = '\0';
+  d.tempnumRows++;
+}
+
+void copyToTemp() {
+  for (int i = 0; i < d.numRows; i++) {
+    if (i > d.tempnumRows) {
+      appendRowTemp(d.row[i].chars, d.row[i].size);
+    } else {
+      d.temprow[i].chars = realloc(d.temprow[i].chars, d.row[i].size);
+      memcpy(d.temprow[i].chars, d.row[i].chars, d.row[i].size);
+    }
+  }
+}
+
 void appendRows(int numrows) {
+  char *s = malloc(d.cols + 1);
+  for (int i = 0; i < d.cols; i++) {
+    s[i] = ' ';
+  }
+  s[d.cols] = '\0';
   while (numrows) {
-    appendRow(NULL, 0);
+    appendRow(s, d.cols);
     numrows--;
   }
 }
@@ -145,8 +217,8 @@ void toggleChar(grow *row, int at) {
 
 // Function to toggle the character at the cursor position between ' ' and '#'
 void toggleCharAtCursor() {
-  if (d.cy >= d.numRows)
-    appendRows(d.cy - d.numRows + 1);
+  if (d.rows - 1 >= d.numRows)
+    appendRows(d.rows - 1 - d.numRows + 1);
   toggleChar(&d.row[d.cy], d.cx);
   if (d.cx <= d.cols - 2)
     d.cx++;
@@ -174,12 +246,42 @@ void openFile(char *filename) {
   fclose(fp);
 }
 
+void saveFile() {
+  if (d.filename == NULL) {
+    d.filename = promptUser("Save as: %s (ESC to cancel)");
+    if (d.filename == NULL) {
+      setStatusMessage("Save aborted.");
+      return;
+    }
+  }
+  int len;
+
+  char *buf = rowsToString(&len);
+
+  int fd = open(d.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != 1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        setStatusMessage("%d bytes written to disk. Filename %s", len,
+                         d.filename);
+        return;
+      }
+    }
+    close(fd);
+  }
+  free(buf);
+}
+
 void initDisplay() {
   d.cx = 0;
   d.cy = 0;
   d.cnt = 0;
   d.numRows = 0;
+  d.tempnumRows = 0;
   d.row = NULL;
+  d.temprow = NULL;
   d.filename = NULL;
   d.statusmsg[0] = '\0';
   d.status_time = 0;
@@ -211,18 +313,28 @@ void enableRawMode() {
 
 /*** output methods ***/
 
+void welcomeMsg(struct buffer *buff, const char *msg) {
+  int len = strlen(msg);
+  if (len > d.cols)
+    len = d.cols;
+  int padding = (d.cols - len) / 2;
+  while (padding--)
+    append(buff, " ", 1);
+  append(buff, msg, len);
+}
+
 void drawRows(struct buffer *buff) {
   for (int i = 0; i < d.rows; i++) {
     if (i >= d.numRows) {
-      if (d.numRows == 0 && i == d.rows / 3) {
-        char helloThere[80];
-        int len = snprintf(helloThere, sizeof(helloThere), "Game of Life");
-        if (len > d.cols)
-          len = d.cols;
-        int padding = (d.cols - len) / 2;
-        while (padding--)
-          append(buff, " ", 1);
-        append(buff, helloThere, len);
+      int start = d.rows / 3;
+      if (d.numRows == 0 && i == start) {
+        welcomeMsg(buff, "Game Of Life");
+      } else if (d.numRows == 0 && i == start + 2) {
+        welcomeMsg(buff, "Move the Cursor with 'h','j','k','l'");
+      } else if (d.numRows == 0 && i == start + 4) {
+        welcomeMsg(buff, "Toggle the character at the cursor with 't'");
+      } else if (d.numRows == 0 && i == start + 6) {
+        welcomeMsg(buff, "Press [ENTER] to start a cycle");
       }
     } else {
       int len = d.row[i].size;
@@ -278,6 +390,7 @@ void refreshDisplay() {
   drawStatusBar(&buff);
   drawMessageBar(&buff);
 
+  setStatusMessage(DFLT_MSG);
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", d.cy + 1, d.cx + 1);
   append(&buff, buf, strlen(buf));
@@ -298,16 +411,104 @@ void setStatusMessage(const char *fmt, ...) {
 
 /*** input methods ***/
 
-char readKeyboardInput() {
-  char c = '\0';
-  if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-    die("read");
-  /*if (iscntrl(c)) {
-    printf("%d\r\n", c);
+char *promptUser(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+
+  size_t len = 0;
+  buf[0] = '\0';
+
+  while (1) {
+    setStatusMessage(prompt, buf);
+    refreshDisplay();
+
+    int c = readKeyboardInput();
+    if (c == DEL_KEY || c == BACKSPACE) {
+      if (len != 0)
+        buf[--len] = '\0';
+    } else if (c == '\x1b') {
+      setStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r') {
+      if (len != 0) {
+        setStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (len == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[len++] = c;
+      buf[len] = '\0';
+    }
+  }
+}
+
+int readKeyboardInput() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+      return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+      return '\x1b';
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '3':
+            return DEL_KEY;
+          case '4':
+            return END_KEY;
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
+      }
+    }
+    return '\x1b';
   } else {
-    printf("%d ('%c')\r\n", c, c);
-  }*/
-  return c;
+    return c;
+  }
 }
 
 void moveCursor(char motion, int cnt) {
@@ -345,6 +546,10 @@ void processKeypress() {
     exit(0);
     break;
 
+  case CTRL_KEY('s'):
+    saveFile();
+    break;
+
   case 'h':
   case 'j':
   case 'k':
@@ -370,6 +575,29 @@ void processKeypress() {
   }
 }
 
+/*** game logic ***/
+
+void executeGeneration() {
+  copyToTemp();
+  for (int i = 0; i < d.numRows; i++) {
+    for (int j = 0; j < d.cols; j++) {
+      int liveNeighbors = 0;
+      if (i > 0) {
+        // check above
+      }
+      if (i < d.numRows) {
+        // check below
+      }
+      if (j > 0) {
+        // check left
+      }
+      if (j < d.cols) {
+        // check right
+      }
+    }
+  }
+}
+
 /*** main ***/
 int main(int argc, char *argv[]) {
   enableRawMode();
@@ -378,7 +606,7 @@ int main(int argc, char *argv[]) {
     openFile(argv[1]);
   }
   //  openFile("testfile");
-  setStatusMessage("HELP: Ctrl-q = quit");
+  setStatusMessage(DFLT_MSG);
   while (1) {
     refreshDisplay();
     processKeypress();
